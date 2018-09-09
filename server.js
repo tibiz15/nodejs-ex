3655a76717aa8f54b29f4f1d99ffa709923114cb
tibiz <sasha.faryna1234@gmail.com>
2018-09-09 22:55:42 +0300

    changed startup page

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
@@ -106,3 +107,15 @@ app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;
+*/
+var http = require('http');
+var fs = require('fs');
+
+
+http.createServer(function (req,res){
+    fs.readFile('body.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+}).listen(8080);
